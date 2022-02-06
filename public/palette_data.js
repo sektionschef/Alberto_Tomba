9f7686464d9f634975268bffdd1b9a4c68ec498e
Stefan Schwaha <the.stevee@gmail.com>
2022-02-06 21:35:14 +0100

    adding palettes

--- public/palette_data.js
+++ public/palette_data.js
@@ -19,5 +19,82 @@ let PALETTE = [
             "#FF643D",
             "#FF3C38"
         ]
-    }
+    },
+    {
+        name: "Chicago Bulgari",
+        values: [
+            "#231942",
+            "#5e548e",
+            "#9f86c0",
+            "#be95c4",
+            "#C3Ce0b1cb3C3"
+        ],
+        url: "https://coolors.co/231942-5e548e-9f86c0-be95c4-e0b1cb"
+    },
+    {
+        name: "Vanillo Rancho",
+        values: [
+            "#F1DAC4",
+            "#A69CAC",
+            "#474973",
+            "#161B33",
+            "#0D0C1D"
+        ],
+        url: "https://coolors.co/f1dac4-a69cac-474973-161b33-0d0c1d"
+    },
+    {
+        name: "no fear of frogs",
+        values: [
+            "#75F4F4",
+            "#90E0F3",
+            "#B8B3E9",
+            "#D999B9",
+            "#D17B88"
+        ],
+        url: "https://coolors.co/75f4f4-90e0f3-b8b3e9-d999b9-d17b88"
+    },
+    {
+        name: "eggs benedict",
+        values: [
+            "#000000",
+            "#5D737E",
+            "#FFF07C",
+            "#F0F7EE",
+            "#87BBA2"
+        ],
+        url: "https://coolors.co/000000-5d737e-fff07c-f0f7ee-87bba2"
+    },
+    {
+        name: "Punk panther",
+        values: [
+            "#104F55",
+            "#32746D",
+            "#9EC5AB",
+            "#01200F",
+            "#011502"
+        ],
+        url: "https://coolors.co/104f55-32746d-9ec5ab-01200f-011502"
+    },
+    {
+        name: "Ahoy, Mr. Tangle",
+        values: [
+            "#004777",
+            "#A30000",
+            "#FF7700",
+            "#EFD28D",
+            "#00AFB5"
+        ],
+        url: "https://coolors.co/004777-a30000-ff7700-efd28d-00afb5"
+    },
+    {
+        name: "Ciao Otto",
+        values: [
+            "#EEE2DF",
+            "#EED7C5",
+            "#C89F9C",
+            "#C97C5D",
+            "#B36A5E"
+        ],
+        url: "https://coolors.co/eee2df-eed7c5-c89f9c-c97c5d-b36a5e"
+    },
 ]
